758855c43696772e552167f88a9dac67e3e1fbed
ashwintumma23 <ashwin.tumma23@gmail.com>
2015-04-28 17:56:49 -0400

    Committed New Files with removed quotes, and <br> characters. Wrote Perl commands for it

--- FetchReviewsAndCreateTSV/createTSVFiles.sh
+++ FetchReviewsAndCreateTSV/createTSVFiles.sh
@@ -9,6 +9,12 @@ do
 	python getReviewsAndLocation.py review_$var\_20.html review_$var\_Output_Location.tsv
 	python getReviewsAndLocation.py review_$var\_30.html review_$var\_Output_Location.tsv
 	python getReviewsAndLocation.py review_$var\_40.html review_$var\_Output_Location.tsv
+	
         echo review_$var\_Output_Location.tsv file written
 done
-
+perl -i'.bak' -p -e 's/<br>//g' *.tsv
+perl -i'.bak' -p -e 's/&quot;//g' review_5_Output_Location.tsv
+perl -i'.bak' -p -e 's/&#..;//g' review_5_Output_Location.tsv
+perl -i'.bak' -p -e 's/&#...;//g' review_5_Output_Location.tsv
+perl -i'.bak' -p -e 's/&amp;/&/g' review_5_Output_Location.tsv
+rm -rf *.bak
